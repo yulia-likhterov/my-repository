bef9b36691e27d48a6301252f87b21a7ff69d26b
Yulia Likhterov <yulia.likhterov@gmail.com>
2019-08-24 13:52:20 +0300

    added calculator class and functions

---
+++ il.co.jb/src/main/java/il/co/jb/Calculator.java
@@ -0,0 +1,22 @@
+package il.co.jb;
+
+public class Calculator {
+
+	public static int add(int a, int b) {
+		return a + b;
+	}
+	
+	public static int substruct (int a, int b) {
+		return a - b;
+	}
+	
+	public static int multiply (int a, int b) {
+		return a * b;
+	}
+	
+	public static double divide (int a, int b) {
+		return a / b;
+	}
+	
+	
+}
--- il.co.jb/src/main/java/il/co/jb/NewTest.java
+++ il.co.jb/src/main/java/il/co/jb/NewTest.java
@@ -1,5 +1,6 @@
 package il.co.jb;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class NewTest {
@@ -8,6 +9,19 @@ public class NewTest {
 	public void test1() {
 		System.out.println("Hello from test1");
 		System.out.println("Testing git...");
+	}
+	
+	@Test
+	public void testCalculatorAdd () {
+		
+		int a = 10;
+		int b = 5;
+		
+		int actualResult = Calculator.add(a, b);
+		int expectedResult = a - b;
+		
+		Assert.assertEquals(actualResult, expectedResult, a + "+" + b + "=" + expectedResult);
+		
 	}
 
 }
